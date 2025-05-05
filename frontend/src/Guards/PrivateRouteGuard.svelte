a3ddb9188b52a732d4c5044703f72801514693b2
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-05 23:38:31 +0200

    validates token before going to privateRoute

--- frontend/src/Guards/PrivateRouteGuard.svelte
+++ frontend/src/Guards/PrivateRouteGuard.svelte
@@ -1,18 +1,28 @@
 <script>
-    import { user } from "../stores/userStore.js";
+    import { user, token } from "../stores/userStore.js";
     import { navigate, useLocation } from "svelte-routing";
     import { onMount } from "svelte";
     const location = useLocation();
-    onMount(() => {
-        if (!$user) {
-            navigate("/",{
+    onMount(async () => {
+        const res = await fetch("/auth/validate-token", {
+            headers: {
+                Authorization: `Bearer ${$token}`
+            }
+        });
+
+        if (res.ok) {
+            const data = await res.json();
+            token.set(data.token);
+        } else {
+            localStorage.removeItem("token");
+            navigate("/", {
                 replace: true,
-                state: { from: location.subscribe }
+                state: {from: location.subscribe}
             });
         }
     });
 </script>
 
 {#if $user}
-    <slot />
+    <slot/>
 {/if}
