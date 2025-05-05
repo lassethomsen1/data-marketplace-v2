79cb4c2fb1db2efda6c255d4a4a8f935cfb73bfe
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-05 20:21:38 +0200

    private routes

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,3 +1,14 @@
-<h1 class="font-bold text-2xl text-center">
-  heelo world
-</h1>
+<script>
+    import {Router, Route} from "svelte-routing";
+    import PrivateRoute from "./Guards/PrivateRoute.svelte";
+</script>
+<Router>
+    <div>
+        <Route path="/">
+            <h1 class="text-9xl text-center">HOME</h1>
+        </Route>
+        <PrivateRoute path="/secret">
+            <h1 class="text-9xl text-center">SECRET</h1>
+        </PrivateRoute>
+    </div>
+</Router>
---
+++ frontend/src/Guards/PrivateRouteGuard.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { user } from "../stores/userStore.js";
+    import { navigate, useLocation } from "svelte-routing";
+    import { onMount } from "svelte";
+    const location = useLocation();
+    onMount(() => {
+        if (!$user) {
+            navigate("/",{
+                replace: true,
+                state: { from: location.subscribe }
+            });
+        }
+    });
+</script>
+
+{#if $user}
+    <slot />
+{/if}
