9796959582667bd6e8f8a3215c710769bdaadb16
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-06 01:23:45 +0200

    added cors handling

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import 'dotenv/config';
 import authRouter from './routers/authRouter.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
