5c4995f702d37093b32763d8b93e1a8116077c28
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-05 20:02:00 +0200

    added express.json & authRouter

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import 'dotenv/config';
+import authRouter from './routers/authRouter.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(authRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
