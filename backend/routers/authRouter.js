05c1cabbd9ed3ff7ef589918ce5fa683b0c3fe7c
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-05 20:00:26 +0200

    added authRouter with signup & login endpoints

---
+++ backend/routers/authRouter.js
@@ -0,0 +1,62 @@
+import { Router } from 'express';
+import { PrismaClient } from '../generated/prisma';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const router = new Router();
+const prisma = new PrismaClient();
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (!user) {
+    return res.status(400).json({ data: 'Invalid credentials' });
+  }
+
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) {
+    return res.status(400).json({ data: 'Invalid credentials' });
+  }
+
+  const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
+    expiresIn: '24h', // for demo purposes
+  });
+
+  res.status(200).json({ token, user: { id: user.id, email: user.email } });
+});
+
+router.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    return res.status(400).json({ data: 'User already exists' });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await prisma.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+    },
+  });
+  const token = jwt.sign({ id: newUser.id, email: newUser.email }, process.env.JWT_SECRET, {
+    expiresIn: '1h',
+  });
+  if (token) {
+    //await sendSignupEmail(newUser.email); todo
+  } else {
+    return res.status(500).json({ data: 'Error signing up' });
+  }
+  res.status(200).json({ token, user: { id: newUser.id, email: newUser.email } });
+});
+
+export default router;
