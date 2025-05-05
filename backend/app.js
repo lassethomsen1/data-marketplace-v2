97852426eab2d3ab976be3df2e7e50818a325891
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-05 19:05:54 +0200

    formatted

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import "dotenv/config";
+import 'dotenv/config';
+
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+  res.send('Hello World!');
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-})
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
