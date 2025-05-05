67d727fb57ead9da70ae64565c3bc9135a1fb6fd
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-05 23:55:46 +0200

    added admin account

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -10,10 +10,22 @@ async function main() {
     create: {
       email: 'user@test.com',
       name: 'User',
+      role: 'USER',
       password: await bcrypt.hash('user', 10),
     },
   });
+  const admin = await prisma.user.upsert({
+    where: { email: 'admin@test.com' },
+    update: {},
+    create: {
+      email: 'admin@test.com',
+      name: 'Admin',
+      role: 'ADMIN',
+      password: await bcrypt.hash('admin', 10),
+    },
+  });
   console.log(user ? 'User created' : 'User already exists');
+  console.log(admin ? 'admin created' : 'admin already exists');
 }
 
 main()
