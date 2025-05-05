05c8bf4904da87a9422c496fc37bd857179f01e6
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-05 20:09:31 +0200

    seeded user

---
+++ backend/prisma/seed.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from '../generated/prisma';
+import bcrypt from 'bcryptjs';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.upsert({
+    where: { email: 'user@test.com' },
+    update: {},
+    create: {
+      email: 'user@test.com',
+      password: await bcrypt.hash('user', 10),
+    },
+  });
+  console.log(user ? 'User created' : 'User already exists');
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async e => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
