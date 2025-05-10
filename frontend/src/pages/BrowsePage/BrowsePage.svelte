66d5ec5bb61cc9276e4f367bfbd937f91cba1ac0
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-10 17:32:44 +0200

    format

--- frontend/src/pages/BrowsePage/BrowsePage.svelte
+++ frontend/src/pages/BrowsePage/BrowsePage.svelte
@@ -42,7 +42,7 @@
 
     <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {#each datasets as data}
-            <ItemCard {...data} />
+            <ItemCard {...data}/>
         {/each}
     </div>
 </div>
