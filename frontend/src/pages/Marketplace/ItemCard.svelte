3ea9464fc068ac142ac5cfcaecc68323a4f9ffe1
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-10 18:40:04 +0200

    made the props to be a item object

--- frontend/src/pages/Marketplace/ItemCard.svelte
+++ frontend/src/pages/Marketplace/ItemCard.svelte
@@ -2,22 +2,22 @@
     import {preferences} from "../../stores/preferenceStore.js";
 
 
-    let {imageUrl, title, shortDescription, tags, author, price} = $props();
+    let { item } = $props();
 </script>
 
 <div class="flex flex-col border border-blue-100 rounded-lg p-4 hover:scale-[1.01] hover:shadow-lg">
     <div class="flex">
-        <h2 class="text-2xl font-semibold text-[#1E40AF] mb-2">{title}</h2>
-        <div class="text-[#1F2937] ml-auto">{price} {$preferences.currency}</div>
+        <h2 class="text-2xl font-semibold text-[#1E40AF] mb-2">{item.title}</h2>
+        <div class="text-[#1F2937] ml-auto">{item.price} {$preferences.currency}</div>
     </div>
-    <p class="text-sm text-[#6B7280] mb-4">By <span class="text-[#0EA5E9] font-medium">{author}</span></p>
-    <p class="text-sm text-[#6B7280] leading-snug line-clamp-3 mb-4">{shortDescription}</p>
+    <p class="text-sm text-[#6B7280] mb-4">By <span class="text-[#0EA5E9] font-medium">{item.author}</span></p>
+    <p class="text-sm text-[#6B7280] leading-snug line-clamp-3 mb-4">{item.shortDescription}</p>
     <div class="flex flex-wrap gap-2 mt-2 mb-2">
-        {#each tags as tag}
+        {#each item.tags as tag}
             <span class="bg-[#F3F4F6] text-xs text-[#1F2937] px-2 py-1 rounded-full">{tag}</span>
         {/each}
     </div>
-    <button class="bg-[#3B82F6] hover:bg-[#1E40AF] text-white text-sm py-2 px-4 rounded-md font-medium transition-all duration-150">
+    <a href="/dataset/:id" class="bg-[#3B82F6] hover:bg-[#1E40AF] text-white text-sm py-2 px-4 rounded-md font-medium transition-all duration-150">
         Buy Now
-    </button>
+    </a>
 </div>
