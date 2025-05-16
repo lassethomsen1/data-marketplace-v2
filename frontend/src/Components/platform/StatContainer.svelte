8589624024a05ff67568cbc1ce394443b8d2fa77
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-16 14:27:59 +0200

    statContainer innit

---
+++ frontend/src/Components/platform/StatContainer.svelte
@@ -0,0 +1,18 @@
+<script>
+    import clsx from 'clsx';
+    const { stat, className = ' '} = $props();
+    // todo change font too bold
+
+    // to avoid bg-gray-300 being overridden by custom bg classes
+    const hasCustomBg = /bg([-\[])/.test(className);
+</script>
+
+<div
+        class={clsx(
+    'inline-block rounded-lg w-auto px-3.5 py-1 font-semibold',
+    !hasCustomBg && 'bg-gray-300',
+    className
+  )}
+>
+    <p>{stat}</p>
+</div>
