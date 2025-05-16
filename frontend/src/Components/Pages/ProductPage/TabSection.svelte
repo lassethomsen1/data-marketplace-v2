ebee2a21b56dfb097c410a0f51ed6cf129bb9182
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-16 14:28:16 +0200

    TabSection for productpage

---
+++ frontend/src/Components/Pages/ProductPage/TabSection.svelte
@@ -0,0 +1,24 @@
+<script>
+    let tabs = $state(['Overview', 'Features', 'Pricing']);
+    let selectedTab = $state('Overview');
+</script>
+<div class="flex space-x-4 border-b-gray-400 border-b mb-4">
+    {#each tabs as tab}
+        <button
+                class={`pb-2 border-b-2 font-medium w-2/3 ${
+        tab === selectedTab ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500'
+      }`}
+                onclick={() => selectedTab = tab}
+        >
+            {tab}
+        </button>
+    {/each}
+</div>
+
+{#if selectedTab === 'Overview'}
+    <p>Here is an overview of the product.</p>
+{:else if selectedTab === 'Features'}
+    <p>Here are the features.</p>
+{:else if selectedTab === 'Pricing'}
+    <p>Check out our pricing plans.</p>
+{/if}
