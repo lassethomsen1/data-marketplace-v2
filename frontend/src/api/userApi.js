e66a7035431617c379ebd3f11e73ecb70ee539b6
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-17 16:56:13 +0200

    user api fetch functions

---
+++ frontend/src/api/userApi.js
@@ -0,0 +1,40 @@
+//todo skal være i userStore
+export async function getUser() {
+  const token = localStorage.getItem('token');
+  const response = await fetch(import.meta.env.VITE_BACKEND_URL + '/auth/user', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+  await handleError(response);
+  const userData = await response.json();
+  return userData.user;
+}
+export async function onboardUser() {
+  const token = localStorage.getItem('token');
+  const response = await fetch(import.meta.env.VITE_BACKEND_URL + '/api/stripe/onboard-seller', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  await handleError(response);
+
+  const userData = await response.json();
+  return userData;
+}
+async function handleError(response) {
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    if (response.status === 403) {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      window.location.href = '/login';
+    }
+    throw new Error(errorMessage || `Request failed with status ${response.status}`);
+  }
+}
