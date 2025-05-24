16693c7e70690da7cc9758b5f2c51ae8f0283a4e
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-24 23:21:38 +0200

    added @ src alias

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
-})
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
