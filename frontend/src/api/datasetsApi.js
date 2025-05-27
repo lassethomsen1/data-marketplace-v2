193abf368080828a599fa6960290be38dc767727
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-27 17:09:34 +0200

    added purchase function

--- frontend/src/api/datasetsApi.js
+++ frontend/src/api/datasetsApi.js
@@ -10,3 +10,18 @@ export async function fetchDataset(datasetID) {
   const datasetData = await response.json();
   return datasetData.dataset;
 }
+export async function purchaseDataset(datasetID) {
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/purchases/${datasetID}`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  await handleError(response);
+  const { checkoutUrl } = await response.json();
+
+  window.location.href = checkoutUrl;
+}
