11932a9d5cfb8705ef49de8d2120b7ea7ca0d917
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-27 16:48:08 +0200

    format dataset util

---
+++ frontend/src/utils/datasetUtil.js
@@ -0,0 +1,13 @@
+export function formatDataset(dataset) {
+  dataset.updatedAt = new Date(dataset.updatedAt).toLocaleDateString();
+
+  if (dataset.filesize > 100_000) {
+    dataset.filesize = (dataset.filesize / 1000).toFixed() + ' MB'; // Convert to MB
+  } else {
+    dataset.filesize = (dataset.filesize / 1024).toFixed() + ' KB'; // Convert to KB
+  }
+
+  dataset.price = '$' + dataset.price / 100; // Format price
+
+  return dataset;
+}
