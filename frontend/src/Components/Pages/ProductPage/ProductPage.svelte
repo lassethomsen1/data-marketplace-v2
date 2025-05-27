9c0eafdbe94b2ee0faa67aa0f53a1d3b94e0ba6c
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-27 16:49:34 +0200

    productpage functionality

--- frontend/src/Components/Pages/ProductPage/ProductPage.svelte
+++ frontend/src/Components/Pages/ProductPage/ProductPage.svelte
@@ -2,52 +2,50 @@
     import StatContainer from "../../platform/StatContainer.svelte";
     import TabSection from "./TabSection.svelte";
     import {onMount} from "svelte";
+    import {fetchDataset} from "@/api/datasetsApi.js";
+    import {formatDataset} from "@/utils/datasetUtil.js";
 
     const {datesetId} = $props();
-    const preferences = {
-        currency: "USD"
-    };
+
 
     let dataset = $state({
-        name: undefined,
-        provider: undefined,
-        size: undefined,
+        title: undefined,
+        description: undefined,
+        filetype: undefined,
+        filesize: undefined,
         price: undefined,
-        format: undefined,
-        sources: undefined,
-        dataPoints: undefined,
-        lastUpdated: undefined
-    })
-    onMount(()=>{
-        dataset = {
-            name: "City Traffic Dataset",
-            provider: "City Data Labs",
-            size: "1.5 GB",
-            price: "$3999",
-            format: "CSV",
-            sources: "City Traffic Cameras, Public Data",
-            dataPoints: "500,000+",
-            lastUpdated: "April 2025"
+        category: undefined,
+        updatedAt: undefined,
+        dataPoints: undefined, // todo skal det muligvis være der ?
+        seller: {
+            name: undefined,
         }
+    })
+    onMount(async () => {
+        dataset = formatDataset(await fetchDataset(datesetId));
+
     });
 </script>
 <div class="flex flex-col justify-center items-center">
     <div class="flex flex-col w-full max-w-3xl">
-        <h1 class="text-4xl">{dataset.name}</h1>
-        <p class="text-gray-400 font-semibold text-xl mb-2">{dataset.provider}</p>
+        <h1 class="text-4xl">{dataset.title}</h1>
+        <p class="text-gray-400 font-semibold text-xl mb-2">{dataset.seller.name}</p>
         <div class="flex flex-row mb-4">
             <div>
-                <StatContainer stat={dataset.size}/>
-                <StatContainer stat={dataset.format}/>
-                <StatContainer stat={dataset.lastUpdated}/>
+                <StatContainer stat={dataset.filesize}/>
+                <StatContainer stat={dataset.filetype}/>
+                <StatContainer stat={dataset.updatedAt}/>
             </div>
             <div class="ml-auto">
                 <StatContainer stat={dataset.price} className={"bg-[#3B82F6] text-white"}/>
-                <button class="bg-[#3B82F6] hover:bg-[#1E40AF] rounded-lg text-white px-6.5 py-1 font-semibold">Purchase</button>
+                <button class="bg-[#3B82F6] hover:bg-[#1E40AF] rounded-lg text-white px-6.5 py-1 font-semibold">
+                    Purchase
+                </button>
             </div>
         </div>
-        <img src="https://placehold.co/600x400" width="600" height="400" alt="sample data table" class="rounded-2xl mx-auto mb-4"/>
-        <TabSection/>
+        <img src="https://placehold.co/600x400" width="600" height="400" alt="sample data table"
+             class="rounded-2xl mx-auto mb-4"/>
+        <TabSection desc={dataset.description}/>
     </div>
 </div>
 
