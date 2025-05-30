48e48c3f481082e67fdce9b2ca9ef9a02b567478
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-05-30 14:06:43 +0200

    refactored to use socket

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,18 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import http from 'http';
 import datasetRouter from './routers/datasetsRouter.js';
 import authRouter from './routers/authRouter.js';
 import stripeRouter from './routers/stripeRouter.js';
 import webhookRouter from './routers/webhookRouter.js';
 import purchaseRouter from './routers/purchaseRouter.js';
+import { initSocket } from './routers/socket/socketServer.js';
 
 const app = express();
 
+const server = http.createServer(app);
+initSocket(server);
 // stripe webhook endpoint must be before express.json middleware
 app.use('/api/stripe', webhookRouter);
 
@@ -23,6 +27,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
