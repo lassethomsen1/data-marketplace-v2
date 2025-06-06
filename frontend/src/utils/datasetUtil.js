5eace8ffe3824f93ac9df148f44531377ec2c67e
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-06-06 14:22:11 +0200

    fixed wrong file size logic

--- frontend/src/utils/datasetUtil.js
+++ frontend/src/utils/datasetUtil.js
@@ -2,10 +2,10 @@ export function formatDataset(dataset) {
   dataset.createdAt = new Date(dataset.createdAt).toLocaleDateString();
   dataset.updatedAt = new Date(dataset.updatedAt).toLocaleDateString();
 
-  if (dataset.filesize > 100_000) {
-    dataset.filesize = (dataset.filesize / 1000).toFixed() + ' MB'; // Convert to MB
+  if (dataset.filesize > 1024 * 1024) {
+    dataset.filesize = (dataset.filesize / (1024 * 1024)).toFixed(2) + ' MB';
   } else {
-    dataset.filesize = (dataset.filesize / 1024).toFixed() + ' KB'; // Convert to KB
+    dataset.filesize = (dataset.filesize / 1024).toFixed(2) + ' KB';
   }
 
   dataset.price = '$' + dataset.price / 100; // price is stored in cents
