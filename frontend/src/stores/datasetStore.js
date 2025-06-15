5e620b9ea82b1e72ba3ed9bdeab18274227e805b
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-06-15 19:59:02 +0200

    added datasetStore for productPage

---
+++ frontend/src/stores/datasetStore.js
@@ -0,0 +1,20 @@
+import { writable } from 'svelte/store';
+import { formatDataset } from '@/utils/datasetUtil.js';
+import { fetchDataset } from '@/api/datasetsApi.js';
+
+export const dataset = writable({
+  id: undefined,
+  title: undefined,
+  description: undefined,
+  filetype: undefined,
+  filesize: undefined,
+  price: undefined,
+  category: undefined,
+  updatedAt: undefined,
+  dataPoints: undefined, // todo skal det muligvis være der ?
+  author: undefined,
+});
+
+export async function fetchDatasetAndUpdate(datasetId) {
+  dataset.set(formatDataset(await fetchDataset(datasetId)));
+}
