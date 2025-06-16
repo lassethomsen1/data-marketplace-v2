0263acfd58ba79f886f0679d5544b0f75f41e6b5
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-06-16 20:45:09 +0200

    fetch upload function

--- frontend/src/api/statsApi.js
+++ frontend/src/api/statsApi.js
@@ -26,3 +26,15 @@ export async function fetchTransactions() {
   await handleError(response);
   return await response.json();
 }
+export async function fetchUploads() {
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/stats/uploads`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+  await handleError(response);
+  return await response.json();
+}
