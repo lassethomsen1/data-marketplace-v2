ac4c268826aa5d04db87cf37c9d354162d0ee857
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-06-16 19:57:49 +0200

    added fetch transactions function

--- frontend/src/api/statsApi.js
+++ frontend/src/api/statsApi.js
@@ -13,3 +13,16 @@ export async function fetchStats() {
   await handleError(response);
   return await response.json();
 }
+
+export async function fetchTransactions() {
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/stats/transactions`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+  await handleError(response);
+  return await response.json();
+}
