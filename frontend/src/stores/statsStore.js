737100fdb26b59016dad70cd42b39c668aa0095b
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-06-16 15:23:27 +0200

    added statStore

---
+++ frontend/src/stores/statsStore.js
@@ -0,0 +1,30 @@
+import { writable } from 'svelte/store';
+import { fetchStats } from '@/api/statsApi.js';
+
+export const stats = writable({
+  database: {
+    users: 0,
+    datasets: 0,
+    failedPurchases: 0,
+    completedPurchases: 0,
+  },
+  stripe: {
+    fees: { total: 0, currency: 'dkk' },
+    totalRevenue: { total: 0, currency: 'usd' },
+  },
+});
+export const statsLoading = writable(false);
+
+export async function fetchAndSetStats() {
+  statsLoading.set(true);
+  try {
+    const data = await fetchStats();
+    stats.set(data);
+    console.log('Stats fetched successfully:', data);
+  } catch (error) {
+    console.error('Failed to fetch stats:', error);
+  } finally {
+    statsLoading.set(false);
+    console.log('Stats loading finished');
+  }
+}
