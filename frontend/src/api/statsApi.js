a3acadd7b8064bb1968fe5f898f0fd53824a4d24
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-06-16 15:23:12 +0200

    added fetch function for stats

---
+++ frontend/src/api/statsApi.js
@@ -0,0 +1,15 @@
+import { handleError } from '@/api/helper/error.js';
+
+export async function fetchStats() {
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/stats`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  await handleError(response);
+  return await response.json();
+}
