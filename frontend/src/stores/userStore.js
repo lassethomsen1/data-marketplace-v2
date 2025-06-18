cd93c139154d65da32c789163d83184d9776a8c4
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-06-18 22:19:42 +0200

    added purchases writable

--- frontend/src/stores/userStore.js
+++ frontend/src/stores/userStore.js
@@ -1,12 +1,15 @@
 import { writable } from 'svelte/store';
 import { fetchUser } from '@/api/userApi.js';
 import { navigate } from 'svelte-routing';
+import { fetchPurchasedDatasets } from '@/api/datasetsApi.js';
+import { formatDataset } from '@/utils/datasetUtil.js';
 
 const savedUser = JSON.parse(localStorage.getItem('user'));
 const savedToken = localStorage.getItem('token');
 
 export const user = writable(savedUser || null);
 export const token = writable(savedToken || null);
+export const purchases = writable([]);
 
 token.subscribe(value => {
   if (value) {
@@ -30,6 +33,19 @@ export async function fetchAndSetUser() {
   } catch (error) {
     console.error('Failed to fetch user:', error);
     user.set(null);
+  }
+}
+
+export async function fetchAndSetPurchases() {
+  try {
+    const formattedPurchases = [];
+    const fetchedPurchases = await fetchPurchasedDatasets();
+    fetchedPurchases.forEach(purchase => {
+      formattedPurchases.push(formatDataset(purchase.dataset));
+    });
+    purchases.set(formattedPurchases);
+  } catch (error) {
+    console.error('Failed to fetch purchases:', error);
   }
 }
 
