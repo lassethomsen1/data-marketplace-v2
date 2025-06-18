9dc2d574eb0cd349d8b41397cc06dfbf6e94f35f
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-06-18 18:45:43 +0200

    added searchDatasets api function

--- frontend/src/api/datasetsApi.js
+++ frontend/src/api/datasetsApi.js
@@ -20,21 +20,39 @@ export async function fetchDatasets() {
   return await response.json();
 }
 
-export async function purchaseDataset(datasetID) {
-  const token = localStorage.getItem('token');
-  const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/purchases/${datasetID}`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
+export async function searchDatasets({
+  search = '',
+  limit = 20,
+  page = 1,
+  category = '',
+  minPrice = 0,
+  maxPrice = null,
+  status = 'AVAILABLE',
+}) {
+  const params = new URLSearchParams({
+    search,
+    limit: limit.toString(),
+    page: page.toString(),
+    category,
+    minPrice: minPrice.toString(),
+    ...(maxPrice !== null ? { maxPrice: maxPrice.toString() } : {}),
+    status,
   });
 
-  await handleError(response);
-  const { checkoutUrl } = await response.json();
+  const response = await fetch(
+    `${import.meta.env.VITE_BACKEND_URL}/api/datasets?${params.toString()}`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
 
-  window.location.href = checkoutUrl;
+  await handleError(response);
+  return await response.json();
 }
+
 export async function fetchPurchasedDatasets() {
   const token = localStorage.getItem('token');
   const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/purchases`, {
@@ -49,6 +67,7 @@ export async function fetchPurchasedDatasets() {
   const { purchases } = await response.json();
   return purchases;
 }
+
 export async function downloadDataset(datasetID) {
   const token = localStorage.getItem('token');
   const response = await fetch(
@@ -65,4 +84,20 @@ export async function downloadDataset(datasetID) {
   await handleError(response);
 
   return await response.json();
+}
+
+export async function purchaseDataset(datasetID) {
+  const token = localStorage.getItem('token');
+  const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/purchases/${datasetID}`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  await handleError(response);
+  const { checkoutUrl } = await response.json();
+
+  window.location.href = checkoutUrl;
 }
