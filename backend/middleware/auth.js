1c2884d7ee9141ebc38815760324cbf4618ebfa2
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-06-18 15:50:57 +0200

    added error message if token == null

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,14 +5,14 @@ export function authenticateToken(req, res, next) {
   const token = req.headers['authorization']?.split(' ')[1];
 
   if (!token) {
-    return res.status(401).json({ message: 'Access Denied: No Token Provided' });
+    return res.status(401).send({ message: 'Access Denied: No Token Provided' });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return res.status(403).json({ message: 'Access Denied: Invalid Token' });
+      return res.status(403).send({ message: 'Access Denied: Invalid Token' });
     }
-    if (token == null) return res.sendStatus(401);
+    if (token == null) return res.status(401).send({ message: 'Access Denied: No Token Provided' });
 
     req.user = user;
     next();
