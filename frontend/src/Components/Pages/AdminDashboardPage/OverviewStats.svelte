efac0997adfbdb2d1655d099c17ea0f2f084b6cf
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-06-19 15:40:51 +0200

    refactored icons to lucide

--- frontend/src/Components/Pages/AdminDashboardPage/OverviewStats.svelte
+++ frontend/src/Components/Pages/AdminDashboardPage/OverviewStats.svelte
@@ -1,5 +1,6 @@
 <script>
   import { stats, statsLoading } from '@/stores/statsStore.js';
+  import {CloudUpload, DollarSign, HardDrive, TrendingUp, TriangleAlert, UserRound} from "lucide-svelte";
   const initialStats = {
     revenue24h: 0,
     revenue7d: 0,
@@ -24,14 +25,7 @@
         <p class="text-2xl font-bold text-gray-900">${$stats.stripe.totalRevenue.total}</p>
       </div>
       <div class="w-12 h-12 bg-blue-100 rounded-full flex items-center justify-center">
-        <svg class="w-6 h-6 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1"
-          ></path>
-        </svg>
+        <DollarSign class="text-blue-600"/>
       </div>
     </div>
   </div>
@@ -43,14 +37,7 @@
         <p class="text-2xl font-bold text-gray-900">{$stats.stripe.fees.total} DKK</p>
       </div>
       <div class="w-12 h-12 bg-teal-100 rounded-full flex items-center justify-center">
-        <svg class="w-6 h-6 text-teal-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
-          ></path>
-        </svg>
+        <TrendingUp class="text-teal-600"/>
       </div>
     </div>
   </div>
@@ -62,14 +49,7 @@
         <p class="text-2xl font-bold text-gray-900">{$stats.database.users}</p>
       </div>
       <div class="w-12 h-12 bg-purple-100 rounded-full flex items-center justify-center">
-        <svg class="w-6 h-6 text-purple-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197m13.5-9a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0z"
-          ></path>
-        </svg>
+        <UserRound class="text-purple-600"/>
       </div>
     </div>
   </div>
@@ -81,14 +61,7 @@
         <p class="text-2xl font-bold text-gray-900">{$stats.database.datasets}</p>
       </div>
       <div class="w-12 h-12 bg-green-100 rounded-full flex items-center justify-center">
-        <svg class="w-6 h-6 text-green-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
-          ></path>
-        </svg>
+        <HardDrive class="text-green-600"/>
       </div>
     </div>
   </div>
@@ -100,14 +73,7 @@
         <p class="text-2xl font-bold text-gray-900">{$stats.database.completedPurchases}</p>
       </div>
       <div class="w-12 h-12 bg-indigo-100 rounded-full flex items-center justify-center">
-        <svg class="w-6 h-6 text-indigo-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-          ></path>
-        </svg>
+        <CloudUpload class="text-indigo-500"/>
       </div>
     </div>
   </div>
@@ -119,14 +85,7 @@
         <p class="text-2xl font-bold text-gray-900">{$stats.database.failedPurchases}</p>
       </div>
       <div class="w-12 h-12 bg-red-100 rounded-full flex items-center justify-center">
-        <svg class="w-6 h-6 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-2.5L13.732 4c-.77-.833-1.964-.833-2.732 0L3.732 16.5c-.77.833.192 2.5 1.732 2.5z"
-          ></path>
-        </svg>
+        <TriangleAlert class="text-red-600"/>
       </div>
     </div>
   </div>
