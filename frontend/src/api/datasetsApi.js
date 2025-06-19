7bf8829a8ff28bb41e6664a4b2076b8a211d1f63
LasseThomsenSkole <lath0003@stud.kea.dk>
2025-06-19 19:12:51 +0200

    added limit to fetched datasets

--- frontend/src/api/datasetsApi.js
+++ frontend/src/api/datasetsApi.js
@@ -12,7 +12,7 @@ export async function fetchDataset(datasetID) {
 }
 
 export async function fetchDatasets() {
-  const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/datasets`, {
+  const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/datasets?limit=6`, {
     method: 'GET',
   });
 
