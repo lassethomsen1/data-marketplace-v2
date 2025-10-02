773efe194239fd74d2ec286d54eb585fc613facc
Lasse Thomsen <lasse.thomsen@hotmail.dk>
2025-10-02 16:00:20 +0200

    fixed a type error

--- v2/backend/middleware/auth.ts
+++ v2/backend/middleware/auth.ts
@@ -3,6 +3,17 @@ import { Response, NextFunction } from 'express';
 import 'dotenv/config';
 import { Users } from "@data/prisma";
 import { authReqDTO } from "../types/ReqDTO";
+import { JwtPayload } from "jsonwebtoken";
+
+
+export interface UserJwtPayload extends JwtPayload {
+  id: Users['id'];
+  email: Users['email'];
+  name: Users['name'];
+  role: Users['role'];
+  stripeAccountId: Users['stripeAccountId'];
+  stripeOnboardingCompleted: Users['stripeOnboardingCompleted'];
+}
 
 export function authenticateToken(req: authReqDTO, res: Response, next: NextFunction) {
   if (!process.env.JWT_SECRET) {
@@ -15,13 +26,22 @@ export function authenticateToken(req: authReqDTO, res: Response, next: NextFunc
     return res.status(401).send({ message: 'Access Denied: No Token Provided' });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user: Users) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       return res.status(403).send({ message: 'Access Denied: Invalid Token' });
     }
     if (token == null) return res.status(401).send({ message: 'Access Denied: No Token Provided' });
 
-    req.user = user;
+    const user = decoded as UserJwtPayload;
+
+    req.user = {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      role: user.role,
+      stripeAccountId: user.stripeAccountId,
+      stripeOnboardingCompleted: user.stripeOnboardingCompleted,
+    };
     next();
   });
 }
