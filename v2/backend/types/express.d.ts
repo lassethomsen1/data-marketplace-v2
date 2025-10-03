629f6640f4bef588edd8fa5b076a621b174bd9e1
Lasse Thomsen <lasse.thomsen@hotmail.dk>
2025-10-03 09:35:27 +0200

    made propeties optional

--- v2/backend/types/express.d.ts
+++ v2/backend/types/express.d.ts
@@ -1,16 +1,14 @@
-// todo: få det her pis fixet
-import 'express';
-import type { Users } from '@data/prisma';
+import { Users } from '@data/prisma';
 
 declare module 'express-serve-static-core' {
   interface Request {
     user?: {
       id: Users['id'];
-      email: Users['email'];
-      name: Users['name'];
-      role: Users['role'];
-      stripeAccountId: Users['stripeAccountId'];
-      stripeOnboardingCompleted: Users['stripeOnboardingCompleted'];
+      email?: Users['email'];
+      name?: Users['name'];
+      role?: Users['role'];
+      stripeAccountId?: Users['stripeAccountId'];
+      stripeOnboardingCompleted?: Users['stripeOnboardingCompleted'];
     };
   }
 }
